38f5011427a782d38a7593cfc450a2b5ef28cf9e
sharankang <sharanpreetkaur2401@gmail.com>
2026-04-21 14:55:41 +0530

    chore: remove unused manual test scripts

--- backend/test_upload.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-with open("test_dummy.pdf", "w") as f:
-    f.write("dummy PDF content")
-
-files = {'file': ('test_dummy.pdf', open('test_dummy.pdf', 'rb'))}
-response = requests.post("http://localhost:8000/api/chat/upload", files=files)
-print("Status:", response.status_code)
-print("Response:", response.json())
